70fc49013747c5c3fe00ade87505452b11d45753
Anders Thøgersen <anders@bladre.dk>
2017-05-21 00:38:12 +0200

    add barcode.js

---
+++ pages/barcode.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Barcode from '../components/barcode';
+import {app} from '../components/app';
+
+export default app((props) => {
+  return (
+    <Barcode>
+    </Barcode>
+  );
+});
